9140cb4924237b64708f907f6283d430445ddb25
geinermt <geinermt22@gmail.com>
2024-10-01 12:58:55 -0500

    se crean los enpoint del controlador

---
+++ backend/src/main/java/com/pintuvalle/servicios/web/controladores/productosCN.java
@@ -0,0 +1,51 @@
+package com.pintuvalle.servicios.web.controladores;
+
+import com.pintuvalle.servicios.web.servicios.interfaz.productosinf;
+
+@Restcontroller
+@Requestmapping("/api/productos")
+public class productosCN {
+   
+    @Autowired
+   private productosinf servicio;
+
+   @Crossorigin(origins = "*")
+    @PostMapping
+    public ResponseEntity<?> listarproductos(){
+        return ResponseEntity.ok(servicio.findAll());
+    }
+
+    @Crossorigin(origins = "*")
+    @GetMapping("/{id}")
+    public ResponseEntity<?> listarproductoById(@PathVariable(value= id)Interger id){
+      Optional<productos> producto = servicio.findById(id);
+     if (producto.isPresent()) {
+         return ResponseEntity.ok(producto);
+      }
+      return ResponseEntity.notFound().build();
+    }
+
+    @Crossorigin(origins = "*")
+    @PostMapping
+    public ResponseEntity<?> crearproducto(@RequestBody productos producto){
+        return ResponseEntity.ok(servicio.save(producto));
+    }
+
+    @Crossorigin(origins = "*")
+    @PutMapping
+    public ResponseEntity<?> editarproducto(@RequestBody productos producto){
+        return ResponseEntity.ok(servicio.save(producto));
+    }
+
+    @Crossorigin(origins = "*")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable(value= id)Interger id){
+      Optional<productos> producto = servicio.findById(id);
+     if (producto.isPresent()) {
+        servicio.delete(id);
+         return ResponseEntity.ok(producto);
+      }
+      return ResponseEntity.notFound().build();
+}
+
+
